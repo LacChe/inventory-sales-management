6138e479a2cae66833b432fb2507511e7a154eae
LacChe <lacche96@gmail.com>
2024-01-09 17:59:45 +0800

    packaging

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function createWindow() {
     width: 1400,
     height: 800,
     autoHideMenuBar: true,
-    icon: 'res/icon.png',
+    icon: 'icon.png',
     webPreferences: {
       nodeIntegration: false,
       worldSafeExecuteJavaScript: true,
@@ -26,8 +26,7 @@ function createWindow() {
 
 if(isDev) {
   require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
-    ignored: path.join(__dirname, 'files')
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
   });
 }
 
@@ -43,15 +42,206 @@ app.on('window-all-closed', function () {
 })
 
 ipcMain.on("readFile", (event, args) => {
-  fs.access(`./files/${args}`, fs.constants.R_OK, e => {
+  fs.access(`./${args}`, fs.constants.R_OK, e => {
     if(e) {
+      let data = [];
       // if file doesnt exist, create one with empty array
-      fs.writeFile(`./files/${args}`, '[]', (error) => {
+      if(args === 'settings.json') data = {
+        "search": "",
+        "filter": "",
+        "inventory": {
+            "order": {
+              "field": "name_en",
+              "asc": true
+            },
+            "showInventoryDataFields": [
+                "name_en",
+                "amount",
+                "country",
+                "size"
+            ],
+            "inventoryDataFields": [
+                {
+                    "name": "id",
+                    "type": "string"
+                },
+                {
+                    "name": "amount",
+                    "type": "formula"
+                },
+                {
+                    "name": "name_en",
+                    "type": "string"
+                },
+                {
+                    "name": "name_cn",
+                    "type": "string"
+                },
+                {
+                    "name": "size",
+                    "type": "string"
+                },
+                {
+                    "name": "manufacturer",
+                    "type": "string"
+                },
+                {
+                    "name": "country",
+                    "type": "string"
+                },
+                {
+                    "name": "reminder_amount",
+                    "type": "number"
+                },
+                {
+                    "name": "notes",
+                    "type": "string"
+                }
+            ]
+        },
+        "product": {
+            "order": {
+              "field": "name_en",
+              "asc": true
+            },
+            "showProductDataFields": [
+                "price",
+                "name_en",
+                "size"
+            ],
+            "productDataFields": [
+                {
+                    "name": "id",
+                    "type": "string"
+                },
+                {
+                    "name": "name_en",
+                    "type": "string"
+                },
+                {
+                    "name": "name_cn",
+                    "type": "string"
+                },
+                {
+                    "name": "size",
+                    "type": "string"
+                },
+                {
+                    "name": "inventory_items",
+                    "type": "dropdown"
+                },
+                {
+                    "name": "price",
+                    "type": "string"
+                },
+                {
+                    "name": "sale_price",
+                    "type": "string"
+                },
+                {
+                    "name": "notes",
+                    "type": "string"
+                }
+            ]
+        },
+        "equipment": {
+            "order": {
+              "field": "name_en",
+              "asc": true
+            },
+            "showEquipmentDataFields": [],
+            "equipmentDataFields": [
+                {
+                    "name": "id",
+                    "type": "string"
+                },
+                {
+                    "name": "name_en",
+                    "type": "string"
+                },
+                {
+                    "name": "name_cn",
+                    "type": "string"
+                },
+                {
+                    "name": "date_bought",
+                    "type": "date"
+                },
+                {
+                    "name": "manufacturer",
+                    "type": "string"
+                },
+                {
+                    "name": "notes",
+                    "type": "string"
+                }
+            ]
+        },
+        "transaction": {
+            "order": {
+                "field": "name_en",
+                "asc": true
+            },
+            "showTransactionDataFields": [
+                "revenue",
+                "date",
+                "amount",
+                "notes",
+                "name_en"
+            ],
+            "transactionDataFields": [
+                {
+                    "name": "id",
+                    "type": "string"
+                },
+                {
+                    "name": "date",
+                    "type": "date"
+                },
+                {
+                    "name": "name_en",
+                    "type": "formula"
+                },
+                {
+                    "name": "name_cn",
+                    "type": "formula"
+                },
+                {
+                    "name": "product_id",
+                    "type": "dropdown"
+                },
+                {
+                    "name": "amount",
+                    "type": "number"
+                },
+                {
+                    "name": "revenue",
+                    "type": "number"
+                },
+                {
+                    "name": "notes",
+                    "type": "string"
+                },
+                {
+                    "name": "not_a_sale",
+                    "type": "boolean"
+                }
+            ]
+        },
+        "chartData": {
+            "records": "",
+            "dateRange": "",
+            "precision": "",
+            "transactions": "",
+            "parameter": ""
+        }
+      }
+      fs.writeFile(`./${args}`, JSON.stringify(data), (error) => {
         if(error){
           console.log("fs write error: ", error);
         } else {
           // then read and send to react
-          fs.readFile(`./files/${args}`, "utf8", (error, data) => {
+          fs.readFile(`./${args}`, "utf8", (error, data) => {
             if(error){
               console.log("fs read error: ", error);
               win.webContents.send("receiveFile", ['error', error]);
@@ -63,7 +253,7 @@ ipcMain.on("readFile", (event, args) => {
       });
     } else {
       // else read and send to react
-      fs.readFile(`./files/${args}`, "utf8", (error, data) => {
+      fs.readFile(`./${args}`, "utf8", (error, data) => {
         if(error){
           console.log("fs read error: ", error);
           win.webContents.send("receiveFile", ['error', error]);
@@ -76,7 +266,7 @@ ipcMain.on("readFile", (event, args) => {
 });
 
 ipcMain.on("saveFile", (event, args) => {
-  fs.writeFile(`./files/${args.filePath}`, JSON.stringify(args.data), (error) => {
+  fs.writeFile(`./${args.filePath}`, JSON.stringify(args.data), (error) => {
     if(error){
       console.log("fs write error: ", error);
     }
