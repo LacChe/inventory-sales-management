c107a6a4d6fbcc3c169554a65510b43b1095ea04
LacChe <lacche96@gmail.com>
2024-02-04 14:55:20 +0800

    remove tailwind class name

--- react-src/index.tsx
+++ react-src/index.tsx
@@ -3,11 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 const App = () => {
-  return (
-    <h1 className="bg-blue-500 text-white p-4">
-      Hello Electron TypeScript React App!
-    </h1>
-  );
+  return <h1>Hello Electron TypeScript React App!</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
