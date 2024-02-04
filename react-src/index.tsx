ef0d62ac058af5d4d7527c2e06458a3a34976f10
LacChe <lacche96@gmail.com>
2024-02-04 14:46:37 +0800

    imported .css

--- react-src/index.tsx
+++ react-src/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 const App = () => {
-  return <h1>Hello Electron TypeScript React App!</h1>;
+  return (
+    <h1 className="bg-blue-500 text-white p-4">
+      Hello Electron TypeScript React App!
+    </h1>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
