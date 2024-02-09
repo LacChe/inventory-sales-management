9b1fbe6a3d599a70f39a685d50556e2d86afeefb
LacChe <lacche96@gmail.com>
2024-02-09 22:52:09 +0800

    remove readfile test

--- react-src/components/App.jsx
+++ react-src/components/App.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import { useStateContext } from "../utils/StateContext";
-import { readFile } from "../utils/eventHandler";
 
 const App = () => {
   // START test useStateContext
-  const state = useStateContext();
-  console.log("state", state);
+  const { inventory } = useStateContext();
+  console.log("inventory", inventory);
   // END test useStateContext
-
-  // START event and file io testing
-  readFileTest();
-  // END event and file io testing
 
   return <h1>Hello Electron React App!</h1>;
 };
-
-async function readFileTest() {
-  // console.log(await readFile("tableSchemas"));
-}
 
 export default App;
