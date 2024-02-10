80e41ac56adc740ef3fa4631d675b879b40c75a7
LacChe <lacche96@gmail.com>
2024-02-10 16:39:03 +0800

    fill in blank fields for products

--- react-src/components/tables/Table.jsx
+++ react-src/components/tables/Table.jsx
@@ -1,41 +1,72 @@
 import React from "react";
+import { fillBlankFromInventory } from "../../utils/dataManip";
 
 const Table = ({ schema, records, userSettings }) => {
   // TODO split into components and export to default and specialized files
+
+  function tableHeader() {
+    return (
+      <thead>
+        <tr>
+          {schema.map((field) => {
+            if (userSettings.hiddenFields.includes(field.name)) return;
+            return <th scope="col">{field.name.replaceAll("_", " ")}</th>;
+          })}
+        </tr>
+      </thead>
+    );
+  }
+
+  function tableData(record, field) {
+    if (field.name === "id") return;
+    if (userSettings.hiddenFields.includes(field.name)) return;
+
+    // TODO display record names instead of ids
+    // display object type by listing key then values
+    if (field.type === "object")
+      return (
+        <td className={field.name}>
+          {Object.keys(record[field.name]).map((key) => {
+            return (
+              <div>
+                {record[field.name][key]} {key}
+              </div>
+            );
+          })}
+        </td>
+      );
+
+    // other field types
+    return <td className={field.name}>{record[field.name]}</td>;
+  }
+
+  function tableBody() {
+    return (
+      <tbody>
+        {Object.keys(records).map((recordId) => {
+          let displayRecord = { ...records[recordId] };
+          displayRecord = fillBlankFromInventory(displayRecord, schema);
+          return (
+            <tr>
+              <th scope="row" className="recordId">
+                {recordId}
+              </th>
+              {schema.map((field) => {
+                return tableData(displayRecord, field);
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  }
+
   return (
     <div>
       <div>buttons</div>
       <table>
-        <thead>
-          <tr>
-            {schema.map((field) => {
-              if (userSettings.hiddenFields.includes(field.name)) return;
-              return <th scope="col">{field.name.replace("_", " ")}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(records).map((recordId) => {
-            return (
-              <tr>
-                <th scope="row" className="recordId">
-                  {recordId}
-                </th>
-                {schema.map((field) => {
-                  if (field.name === "id") return;
-                  if (userSettings.hiddenFields.includes(field.name)) return;
-                  return (
-                    <td className={field.name}>
-                      {typeof records[recordId][field.name] === "object"
-                        ? JSON.stringify(records[recordId][field.name])
-                        : records[recordId][field.name]}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
+        {tableHeader()}
+        {tableBody()}
       </table>
     </div>
   );
--- react-src/utils/dataManip.js
+++ react-src/utils/dataManip.js
@@ -1,1 +1,21 @@
 // filtering, sorting, filling in blank fields
+import { useStateContext } from "../utils/StateContext";
+
+export function fillBlankFromInventory(record, schema) {
+  const { inventory } = useStateContext();
+  if (Object.keys(record.inventory_items || {}).length <= 0) return record;
+  if (!inventory) return record;
+
+  let newRecord = { ...record };
+
+  schema.forEach((field) => {
+    if (field.fill_blank_from_inventory) {
+      newRecord[field.name] = field.fill_blank_from_inventory
+        .map((invField) => {
+          return inventory[Object.keys(newRecord.inventory_items)[0]][invField];
+        })
+        .join(" ");
+    }
+  });
+  return newRecord;
+}
